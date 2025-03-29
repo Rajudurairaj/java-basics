51cf0722f74f117e68ed675ae744d80224bc7539
Rajudurairaj <rajudurairaj2001@gmail.com>
2025-03-29 12:41:12 +0530

    oops

---
+++ p38.java
@@ -0,0 +1,35 @@
+package java25;
+import java.util.*;
+public class p38 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+pp38 opp38 = new pp38();
+ppp38 oppp38 = new ppp38();
+String name=oppp38.fppp38();
+int age=opp38.fpp38();
+System.out.println("Name is : "+name);
+System.out.println("Age is : "+age);
+
+
+	}
+
+}
+class pp38{
+	public int fpp38() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a your age :");
+		int age=sc.nextInt();
+		return age;
+	}
+}
+class ppp38{
+	public String fppp38() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a your name :");
+		String name=sc.nextLine();
+		return name;
+		
+	}
+	
+}
