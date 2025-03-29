e80973eaa41eefa52858eedfa6e757aaef7abeec
Rajudurairaj <rajudurairaj2001@gmail.com>
2025-03-29 10:56:59 +0530

    string

---
+++ p35.java
@@ -0,0 +1,11 @@
+package java25;
+
+public class p35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str1 = "Hello team good morning everyone";
+System.out.println(str1.replaceAll(" ",""));
+	}
+
+}
