d0732e51ac79861872c94306a31f996ff174fa83
Rajudurairaj <rajudurairaj2001@gmail.com>
2025-03-29 10:45:40 +0530

    string

---
+++ p34.java
@@ -0,0 +1,20 @@
+package java25;
+
+public class p34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String team1="Mumbai Indians";
+System.out.println(team1.charAt(0));
+System.out.println(team1.substring(3,7));
+System.out.println(team1.indexOf('m'));
+int var = team1.lastIndexOf(team1);
+System.out.println(var);
+System.out.println(team1.lastIndexOf(team1));
+System.out.println(team1.toLowerCase());
+System.out.println(team1.toUpperCase());
+System.out.println(team1.replaceAll("i", "I"));
+
+	}
+
+}
