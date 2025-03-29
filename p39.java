baa5d1fca901c957cbeebe9e55f66cfa832fde51
Rajudurairaj <rajudurairaj2001@gmail.com>
2025-03-29 13:02:11 +0530

    oops

---
+++ p39.java
@@ -0,0 +1,15 @@
+package java25;
+
+public class p39 {
+static String name="CSK";
+static int age =25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+p39 op39=new p39();
+System.out.println(op39.name);
+System.out.println(op39.age);
+System.out.println(p39.name);
+System.out.println(p39.age);
+	}
+
+}
---
+++ p40.java
@@ -0,0 +1,14 @@
+package java25;
+
+public class p40 {
+	
+static String name="CSK";
+static int age =25;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(name);
+System.out.println(age);
+	}
+
+}
